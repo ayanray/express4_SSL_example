2ee8e12a48e12d31c4a9763b68faa7ea8ff0aab3
Ayan Ray <ayan@ayanray.com>
2016-01-16 11:39:58 -0800

    working example, big commit :O

---
+++ app.js
@@ -0,0 +1,44 @@
+var fs = require("fs");
+var express = require("express");
+var https = require('https');
+var http = require('http');
+
+/////////////////////////////////////////////
+
+var HTTP_PORT = 3102;
+var HTTPS_PORT = 3101;
+
+/////////////////////////////////////////////
+
+var app = express();
+
+// Route all Traffic to Secure Server
+// Order is important (this should be the first route)
+app.all('*', function(req, res, next){
+  if (req.secure) {
+    return next();
+  };
+  res.redirect('https://localhost:'+HTTPS_PORT+req.url);
+  // res.redirect('https://'+req.hostname+':'+HTTPS_PORT+req.url);
+});
+
+// Hello World
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+/////////////////////////////////////////////
+// Setup Servers
+
+// HTTPS
+var secureServer = https.createServer({
+    key: fs.readFileSync('private.key'),
+    cert: fs.readFileSync('certificate.pem')
+  }, app)
+  .listen(HTTPS_PORT, function () {
+    console.log('Secure Server listening on port ' + HTTPS_PORT);
+});
+
+var insecureServer = http.createServer(app).listen(HTTP_PORT, function() {
+  console.log('Insecure Server listening on port ' + HTTP_PORT);
+})
